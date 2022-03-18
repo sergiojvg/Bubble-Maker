36df91f4e2064cb679796bbd74fd605cb4967ea6
Sergio Vega <sergiojvg92@gmail.com>
2022-03-18 17:49:34 +0100

    feat: Add key handling

--- app/app.py
+++ app/app.py
@@ -4,28 +4,77 @@ try:
 except ImportError:
     print("==> RPI.GPIO library not found. Make sure to run this app with -t flag")
 
+from pynput.keyboard import Key, Listener, KeyCode
 import time
 import getopt, sys
 
 class submarine:
-    def __init__(self,leftPin):
-        self.leftPin = leftPin
+    def __init__(self):
+        LEFT_FORWARD = 16
+        LEFT_BACKWARD = 16
         self.pins = {
-            "left": leftPin
+            "leftForward": LEFT_FORWARD,
+            "leftBackward": LEFT_BACKWARD
         }
 
         if dry_run == False:
             GPIO.setmode(GPIO.BOARD)
             for pin in pins.values():
                 GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, False)
+        else:
+            self.pinsMocker = {}
+            for pin in self.pins.keys():
+                self.pinsMocker[pin] = False
+            print("- Pins Mocker initialized with values: " + str(self.pinsMocker))
         #for pin in [leftPin]:
         #    GPIO.setup(pin, GPIO.OUT)
 
-    def printInformation(self):
+    def printConfiguration(self):
         print("=> Submarine config:")
         for pinName in self.pins.keys():
             print("---- Pin for " + pinName + " is configured on pin number " + str(self.pins[pinName]) )
 
+    def printStatus(self):
+        if dry_run == True:
+            print("Currently, pins are configured like this: ")
+            print("- Pins Mocker: " + str(self.pinsMocker))
+
+    def startTurnRight(self):
+        self.setPin(pinName="leftForward",value=True)
+
+    def stopTurnRight(self):
+        self.setPin(pinName="leftForward",value=False)
+
+    def setPin(self, pinName, value):
+        if dry_run == False:
+            GPIO.output(self.pins[pinName], value)
+        else:
+            self.pinsMocker[pinName] = value
+
+
+
+def key_pressed_handler(key):
+    if hasattr(key, 'char'):
+        match key.char:
+            case "q":
+                return False
+            case "i":
+                bubbles.printConfiguration()
+                bubbles.printStatus()
+    else:
+        match key:
+            case Key.esc:
+                return False
+            case Key.right:
+                bubbles.startTurnRight()
+        bubbles.printStatus()
+
+
+def key_released_handler(key):
+    match key:
+        case Key.right:
+            bubbles.stopTurnRight()
 
 if __name__ == "__main__":
     global dry_run
@@ -53,8 +102,13 @@ if __name__ == "__main__":
     print("==> Application started")
     print("==> Initializing bubbles")
 
-    bubbles = submarine(leftPin=16)
-    bubbles.printInformation()
+    bubbles = submarine()
+    bubbles.printConfiguration()
+    
+    with Listener(
+            on_press = key_pressed_handler,
+            on_release = key_released_handler) as listener:
+        listener.join()
 
 
 
